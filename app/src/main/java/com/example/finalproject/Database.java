cc6fe914569f7e2d70ca8ca9e24d8a9cd8efadff
jaycooo <j.coronado@live.com>
2025-06-28 18:18:32 -0400

    Enhancement 2

--- app/src/main/java/com/example/finalproject/Database.java
+++ app/src/main/java/com/example/finalproject/Database.java
@@ -1,11 +1,10 @@
 package com.example.finalproject;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +23,12 @@ public class Database extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_INVENTORY + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_NAME + " TEXT, " +
-                COLUMN_QUANTITY + " INTEGER)";
+                COLUMN_NAME + " TEXT NOT NULL, " +
+                COLUMN_QUANTITY + " INTEGER NOT NULL DEFAULT 0)";
         db.execSQL(createTable);
     }
 
@@ -45,47 +40,45 @@ public class Database extends SQLiteOpenHelper {
 
     // Insert an item
     public void addItem(InventoryItem item) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_NAME, item.getName());
-        values.put(COLUMN_QUANTITY, item.getQuantity());
-        db.insert(TABLE_INVENTORY, null, values);
-        db.close();
+        try (SQLiteDatabase db = this.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NAME, item.getName());
+            values.put(COLUMN_QUANTITY, item.getQuantity());
+            db.insert(TABLE_INVENTORY, null, values);
+        }
     }
 
     // Get all items
     public List<InventoryItem> getAllItems() {
         List<InventoryItem> items = new ArrayList<>();
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_INVENTORY, null, null, null, null, null, null);
+        String query = "SELECT " + COLUMN_NAME + ", " + COLUMN_QUANTITY + " FROM " + TABLE_INVENTORY;
 
-        if (cursor.moveToFirst()) {
-            do {
-                int id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID));
-                String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME));
-                int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_QUANTITY));
+        try (SQLiteDatabase db = this.getReadableDatabase();
+             Cursor cursor = db.rawQuery(query, null)) {
+
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(0);
+                int quantity = cursor.getInt(1);
                 items.add(new InventoryItem(name, quantity));
-            } while (cursor.moveToNext());
+            }
         }
-        cursor.close();
-        db.close();
+
         return items;
     }
 
-    // Delete an item
+    // Delete an item by name
     public void deleteItem(String name) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_INVENTORY, COLUMN_NAME + "=?", new String[]{name});
-        db.close();
+        try (SQLiteDatabase db = this.getWritableDatabase()) {
+            db.delete(TABLE_INVENTORY, COLUMN_NAME + " = ?", new String[]{name});
+        }
     }
 
-    // Update an item
+    // Update an item's quantity
     public void updateItem(String name, int newQuantity) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_QUANTITY, newQuantity);
-        db.update(TABLE_INVENTORY, values, COLUMN_NAME + "=?", new String[]{name});
-        db.close();
+        try (SQLiteDatabase db = this.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_QUANTITY, newQuantity);
+            db.update(TABLE_INVENTORY, values, COLUMN_NAME + " = ?", new String[]{name});
+        }
     }
 }
-
